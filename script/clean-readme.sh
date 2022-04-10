4295408f7aa51644d79d00cdfbb3e76be9895a73
杏梢 <73057402+hacamer@users.noreply.github.com>
2022-04-10 23:05:16 +0800

    Update clean-readme.sh

--- script/clean-readme.sh
+++ script/clean-readme.sh
@@ -5,7 +5,7 @@ num_al=`sed -n 's/^! Total count: //p' allow.txt`
 num_adb=`sed -n 's/^! Total count: //p' adblock.txt`
 num_adp=`sed -n 's/^! Total count: //p' adblock+adguard.txt`
 num_dns=`sed -n 's/^! Total count: //p' dns.txt`
-num_hosts=`sed -n 's/^! Total count: //p' hosts.txt`
+num_hosts=`sed -n 's/^# Total count: //p' hosts.txt`
 num_domains=`sed -n 's/^! Total count: //p' ad-domains.txt`
 
 time=$(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')
